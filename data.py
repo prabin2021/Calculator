24ed6552893a4f816a0542e31038c820fd4b1082
Prabin <sigdelprabin321@gmail.com>
2026-01-02 20:08:05 +0545

    Branch changes

--- data.py
+++ data.py
@@ -1,11 +1,11 @@
-# DATA 
+# # DATA 
 # Two types -> Numerical [7, 9, -5, 2.5 ] and String [ "Hello", "Prabin" , "sigdel.....32@gmail.com"]
 # Numerical -> Integer[9, 5, -3, 0, 12] and Float(Decimal) [ 2.5 , 3.14 , -9.8]
 # Boolean data -> True or False
 
 # a= 10          # Integer
 # b= 5.5         # Float
-# name= "Prabin" # String
+# name= "Ram" # String
 # z = True      # Boolean
 # print(type(a))     
 # print(type(b))      
